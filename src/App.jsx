cb7e9e181ffc2cbbb5bce45fa406c668903721da
Dennis moseti <dudubot404@gmail.com>
2023-07-17 05:41:43 -0400

    ui & func: ui clean up with basic firebase api calls

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,148 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { useState, useEffect } from 'react'
+// import reactLogo from './assets/react.svg'
+// import viteLogo from '/vite.svg'
 import './App.css'
+import { firebaseConfig } from './config/config'
+
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app"
+import { getFirestore, collection, getDocs } from "firebase/firestore"
+
+import AppBar from './components/AppBar'
+import SideBar from './components/SideBar'
+import Content from './components/Content'
+
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig)
+const db = getFirestore(app)
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [tab, setTab] = useState(0)
+  const [users, setUsers] = useState([])
+  const [specialists, setSpecialists] = useState([])
+  const [schedules, setSchedules] = useState([])
 
+  // functions
+  const usersData = async() => {
+    // get data about users
+    try {
+      const querySnap = await getDocs(collection(db, 'users'))
+      if(querySnap.empty){
+        // do data
+        return
+      }
+      var items = []
+      querySnap.forEach((doc)=>{
+        const dt = doc.data()
+        var item ={
+          id: doc.id,
+          name: dt.name,
+          telephone: dt.telephone,
+          email: dt.email,
+          at: dt.at.toDate()
+        }
+        items.push(item)
+      })
+      setUsers([...items])
+    } catch (error) {
+      alert(`An error occured:, ${error}`)
+    }
+  }
+
+  const specialistsData = async()=>{
+    // get data about specialists
+    try {
+      const querySnap = await getDocs(collection(db, 'specialists'))
+      if(querySnap.empty){
+        // do data
+        return
+      }
+      var items = []
+      querySnap.forEach((doc)=>{
+        const dt = doc.data()
+        var item ={
+          id: doc.id,
+          location: dt.location,
+          speciality: dt.speciality,
+          cert: dt.cert,
+          profile: dt.profile,
+          name: dt.name,
+          reg: dt.regNo
+        }
+        items.push(item)
+      })
+      setSpecialists([...items])
+    } catch (error) {
+      alert(`An error occured:, ${error}`)
+    }
+  }
+
+  const schedulesData = async()=>{
+    // get data about schedules
+    try {
+      const querySnap = await getDocs(collection(db, 'schedules'))
+      if(querySnap.empty){
+        // do data
+        return
+      }
+      var items = []
+      querySnap.forEach((doc)=>{
+        const dt = doc.data()
+        var item ={
+          id: doc.id,
+          patient: dt.patient,
+          specialist: dt.specialist,
+          online: dt.online,
+          at: dt.at.toDate()
+        }
+        items.push(item)
+      })
+      setSchedules([...items])
+    } catch (error) {
+      alert(`An error occured:, ${error}`)
+    }
+  }
+
+  const tabChange = (index)=>{
+    switch (index) {
+      case 0:
+        // home
+        break
+      case 1:
+        // users
+        break
+      case 2:
+        // specialists
+        break
+      case 3:
+        // schedules
+        break
+      case 4:
+        // Registrations
+        break
+      default:
+        break
+    }
+    setTab(index)
+  }
+
+  // effects
+  useEffect(() => {
+    usersData()
+    specialistsData()
+    schedulesData()
+  }, [])
+  
+  // ui
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <div className='main'>
+      <AppBar />
+      <div className='content'>
+        <SideBar tab={tab} handler={tabChange}/>
+        <Content tab={tab} users={users} specialists={specialists} schedules={schedules}/>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    </div>
   )
 }
 
